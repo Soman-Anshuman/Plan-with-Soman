979dc802766d568a3e7ba913ed9c6225853e96f6
Soman-Anshuman <103095714+Soman-Anshuman@users.noreply.github.com>
2024-10-02 00:21:59 +0530

    Modified background

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -4,6 +4,7 @@ import FilterButtons from "./FilterButtons";
 function Tours({tours, removeTour, filter, onFilterChange}){
     return (
         <div className="container">
+            <img src="aesthetic-wallpaper.jpg" className="background" alt="back"></img>
             <h2 className="title">Plan With Soman</h2>
 
             <FilterButtons filter={filter} onFilterChange={onFilterChange} />
