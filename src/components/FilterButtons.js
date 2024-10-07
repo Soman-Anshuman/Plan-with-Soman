d4efa6c29e781eefeac2224e35a5faa0148eaf97
Soman-Anshuman <103095714+Soman-Anshuman@users.noreply.github.com>
2024-10-08 00:13:05 +0530

    Update class of FilterButtons

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -4,11 +4,11 @@ const FilterButtons = ({ filter, onFilterChange }) => {
   return (
     <div className="filter-buttons">
       {/* Filter Buttons */}
-      <button onClick={() => onFilterChange('all')} className={filter === 'all' ? 'active' : ''}>All</button>
-      <button onClick={() => onFilterChange('east')} className={filter === 'east' ? 'active' : ''}>East</button>
-      <button onClick={() => onFilterChange('west')} className={filter === 'west' ? 'active' : ''}>West</button>
-      <button onClick={() => onFilterChange('north')} className={filter === 'north' ? 'active' : ''}>North</button>
-      <button onClick={() => onFilterChange('south')} className={filter === 'south' ? 'active' : ''}>South</button>
+      <button onClick={() => onFilterChange('all')} className={filter === 'all' ? 'active all' : 'all'}>All</button>
+      <button onClick={() => onFilterChange('east')} className={filter === 'east' ? 'active east' : 'east'}>East</button>
+      <button onClick={() => onFilterChange('west')} className={filter === 'west' ? 'active west' : 'west'}>West</button>
+      <button onClick={() => onFilterChange('north')} className={filter === 'north' ? 'active north' : 'north'}>North</button>
+      <button onClick={() => onFilterChange('south')} className={filter === 'south' ? 'active south' : 'south'}>South</button>
     </div>
   );
 };
